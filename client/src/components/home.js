622cf78578e72ca12dabad5c0cc73bb01c25bc2f
seba-pas <sebastian.pastorenzi@gmail.com>
2022-06-26 21:13:59 -0300

    Starting Front-End

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,19 +1,31 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import { getDogs } from '../actions';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getDogs } from "../actions";
 
 
+export default function Home() {
+  // guardo los hooks en las constantes
+  const dispatch = useDispatch();
+  const allDogs = useSelector((state) => state.dogs);
 
-export default function Home () {
+  //dispatch en el componentDidMount
+  useEffect(() => {
+    dispatch(getDogs());
+  }, []);
 
+  //funcion para volver a cargar los perros (salir de los filtros, etc)
+  function handleClick(e) {
+    e.preventDefault();
+    dispatch(getDogs());
+  }
 
-    const dispatch = useDispatch();
-    conste allDogs =useSelector(state => state.dogs)
-
-
-
+  // render del componente
   return (
-    <div>home</div>
-  )
+    <div>
+      <link to="/dogs">Crear nueva raza</link>
+      <h1>Bienevenido a perros</h1>
+      <button onClick={(e) => handleClick(e)}>Volver a cargar perros</button>
+    </div>
+  );
 }
