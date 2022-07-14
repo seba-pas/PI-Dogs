b5cd6f49a4fe1f90ad6ccc24159423718b1318be
seba-pas <sebastian.pastorenzi@gmail.com>
2022-07-14 20:39:58 -0300

    READY FOR DEPLOYMENT

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ const { conn } = require("./src/db.js");
 
 
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log("Back-End listening"); // eslint-disable-line no-console
   });
